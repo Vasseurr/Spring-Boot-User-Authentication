631fd9ef4b4ec75768ad2ce8433aef57bd4b921f
Okan <67058617+Vasseurr@users.noreply.github.com>
2021-02-25 19:50:31 +0300

    add register form
    -register form
    -index html
    -changing into user controller

--- src/main/java/com/vasseurr/registrationLogin/controller/UserController.java
+++ src/main/java/com/vasseurr/registrationLogin/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.vasseurr.registrationLogin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.vasseurr.registrationLogin.model.User;
 
 @Controller
 public class UserController {
@@ -9,6 +12,12 @@ public class UserController {
 	@GetMapping("/")
 	public String viewHomePage() {
 		return "index";
+	}
+	
+	@GetMapping("/register")
+	public String showSignUpForm(Model model) {
+		model.addAttribute("user", new User());
+		return "signup_form";
 	}
 	
 }
