af71e98ba9709ee693868f75edd763738177bcbe
Okan <67058617+Vasseurr@users.noreply.github.com>
2021-05-07 02:57:13 +0300

    updated sql

--- userRecord.sql
+++ userRecord.sql
@@ -1,6 +1,6 @@
 use userlogindb;
-INSERT INTO user(email, password, first_name, last_name)VALUES("jackmic@gmail.com", "jack123", "Jack", "Mic");
-INSERT INTO user(email, password, first_name, last_name)VALUES("Harrison45@gmail.com", "harrison12", "Harrison", "Michael");
-INSERT INTO user(email, password, first_name, last_name)VALUES("AlexRob@gmail.com", "alexrobb", "Alex", "Robby");
+#INSERT INTO user(email, password, first_name, last_name)VALUES("jackmic@gmail.com", "jack123", "Jack", "Mic");
+#INSERT INTO user(email, password, first_name, last_name)VALUES("Harrison45@gmail.com", "harrison12", "Harrison", "Michael");
+#INSERT INTO user(email, password, first_name, last_name)VALUES("AlexRob@gmail.com", "alexrobb", "Alex", "Robby");
 
 SELECT * FROM userlogindb.user;
